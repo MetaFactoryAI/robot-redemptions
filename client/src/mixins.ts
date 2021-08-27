4846265178f0178ff26953a74cc3c7bf1488a177
Hammad Jutt <jutt@ualberta.ca>
2021-08-26 20:52:12 -0600

    August 2021 Distribution

--- client/src/mixins.ts
+++ client/src/mixins.ts
@@ -9,7 +9,8 @@ const modules = Object.entries(store.state).map(module => module[0]);
 
 const AIRDROP_MONTH_MAP = {
   1: 'April 2021 Distribution',
-  2: 'June 2021 Distribution'
+  2: 'June 2021 Distribution',
+  3: 'August 2021 Distribution'
 }
 
 export default {
